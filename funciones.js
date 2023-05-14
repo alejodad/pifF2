707e9e3f2442afc5a6e988428d62923b675f8514
alejodad03 <209984-alejodad03@users.noreply.replit.com>
2023-05-14 22:19:28 +0000

    1ra version

---
+++ funciones.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+  $('#metodo').change(function() {
+    var seleccion = $(this).val();
+    var contenedorInputs = $('#contenedorInputs');
+    contenedorInputs.empty();
+    if (seleccion == 1) {    
+      contenedorInputs.append('<div class="mb-3"><label for="cantidad" class="form-label" >Cantidad de cargas</label><input type="number" id="cantidad" name="cantidad" class="form-control"  min="2" required></div>');
+      
+  $('input[name="cantidad"]').on('change', function() {
+        var cantidad = $(this).val();
+     // Limpiar cualquier input previamente generado
+    contenedorInputs.append('<div class="pricing-header px-3 py-3 pb-md-4 mx-auto text-center"><p class="lead">Genial ahora indicanos los valores de las cargas en Coulombs y su distancia en metros</p></div>');
+        for (var i = 1; i <= cantidad; i++) {
+           contenedorInputs.append('<div class="row pb-3"><div class="col-md-6"><div class="row"><div class="col-6 pe-0"><label class="form-label" for="carga' + i + '">Carga ' + i + ':</label><input type="number" name="carga' + i + '" id="carga' + i + '" class="form-control text-end pe-0" step="0.0001" required=""></div>  <div class="col-1 pt-4 ps-0 pe-0">    <label class="form-label" for=""></label><span class="input-group-text" id="">^</span></div><div class="col-5 ps-0"><label class="form-label" for="potenciaCarga' + i + '">Potencia Carga ' + i + ':</label><input type="number" name="potenciaCarga' + i + '" id="potenciaCarga' + i + '" class="form-control text-end " step="0.0001" required=""></div></div></div><div class="col-md-6"><label class="form-label" for="dCarga' + i + '">Distancia carga ' + i + ':</label><input type="number" name="dCarga' + i + '" id="dCarga' + i + '" class="form-control text-end " step="0.0001" required></div></div>');
+        }
+    contenedorInputs.append('<div class="pricing-header px-3 py-3 pb-md-4 mx-auto text-center"><p class="lead">Por último indica si deseas ver el potencial por cada carga</p></div>');
+    });
+    }
+    else{
+      contenedorInputs.append('<div class="mb-3"><div class="row"><div class="col-6 pe-0"><label class="form-label" for="lambda">Lambda:</label><input type="number" name="lambda" id="lambda" class="form-control text-end pe-0" step="0.0001" required=""></div>  <div class="col-1 pt-4 ps-0 pe-0">    <label class="form-label" for=""></label><span class="input-group-text" id="">^</span></div><div class="col-5 ps-0"><label class="form-label" for="potenciaCarga1">Potencia Lmabda:</label><input type="number" name="potenciaCarga1" id="potenciaCarga1" class="form-control text-end " step="0.0001" required=""></div></div></div><div class="mb-3"><label for="radio" class="form-label" >Radio</label><input type="number" id="radio" name="radio" class="form-control text-end "  " required></div><div class="mb-3"><label for="distancia" class="form-label" >Distancia</label><input type="number" id="distancia" name="distancia" class="form-control text-end "  required></div>');
+      
+    }
+    });
+      
+});
+
+
+function validarMetodo(){
+  var miSelect = $('#metodo');
+  if (miSelect.val() === '') {Swal.fire({
+  icon: 'error',
+  title: 'Oops...',
+  text: 'Debe seleccionar el metodo!'
+})
+    return;
+  }else if (verificarInputs()){
+    $.post("procesaResultado.php", $("#data").serialize(), function(data){
+      Swal.fire(data);
+         //$('#divEspecifico').html(data);
+     });
+  }
+}
+
+
+function verificarInputs() {
+  // Obtener todos los inputs de la página
+  const inputs = document.querySelectorAll('input');
+  
+  // Verificar si todos los inputs tienen un valor
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].value === '') {
+      Swal.fire({
+  icon: 'error',
+  title: 'Oops...',
+  text: 'Debe completar todos los campos antes de continuar!'
+})
+      return false;
+    }
+  } 
+  // Si todos los inputs tienen un valor, continuar con la ejecución
+  return true;
+}
+
--- index.php
+++ index.php
@@ -1,15 +1,44 @@
 <html>
   <head>
-    <title>PHP Test</title>
+    <meta charset="utf-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+  <meta name="viewport" content="width=device-width">
+    <title>PIF Fisca</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="funciones.js"></script>
+    
   </head>
   <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+    <div class="container">
+      <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h1 class="display-4">Bienvenido</h1>
+<p class="lead">En este pequeño sistema podrás ingresar ejercicios usando los métodos: Newton, Falsa Posición, Secante y Bisección.</p>
+        <p class="lead">Primero debes ingresar la cantidad de cargas que deseas operar.</p>
+         </div>
+          <form  method="post" id="data">
+            <div class="mb-3">
+<select class="form-select" aria-label="Default select example" id="metodo" name ="metodo">
+  <option selected value="">Seleccione metodo</option>
+  <option value="1">Potencial Carga discreta</option>
+  <option value="2">Potencial Carga continua</option>
+</select>
+   <!--<input type="text" name="expresion" class="form-control"> -->
+  </div>
+          
+          <div id="contenedorInputs">
 
-  <!--
-  This script places a badge on your repl's full-browser view back to your repl's cover
-  page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
-  teal, blue, blurple, magenta, pink!
-  -->
+          
+            
+          </div>
+            <div class="mb-3">
+    <button type="button" class="btn btn-success form-control" value="Iniciar"onclick="validarMetodo()" >Enviar formulario</button>
+          </div>
+        </form>
+    </div>
+        
   <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
+    <div id="divEspecifico"></div>
   </body>
 </html>
